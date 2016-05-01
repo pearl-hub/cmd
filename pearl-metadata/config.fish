136d94952d76ba71fee095105797f6240fc76c1b
Filippo Squillace <squillace.filippo@gmail.com>
2016-05-01 19:32:12 +0100

    Add path to PATH only once

--- pearl-metadata/config.fish
+++ pearl-metadata/config.fish
@@ -1,4 +1,6 @@
-set PATH $PATH $PEARL_PKGDIR/bin
+if not contains $PEARL_PKGDIR/bin $PATH
+    set PATH $PATH $PEARL_PKGDIR/bin
+end
 
 # Trap USR2 signal
 trap "$PEARL_TEMPORARY/new_cmd" USR2
--- pearl-metadata/config.sh
+++ pearl-metadata/config.sh
@@ -1,4 +1,7 @@
-PATH=$PATH:$PEARL_PKGDIR/bin
+if [[ $PATH != *"${PEARL_PKGDIR}/bin"* ]]
+then
+    PATH=$PATH:$PEARL_PKGDIR/bin
+fi
 
 # Trap USR2 signal
 trap "$PEARL_TEMPORARY/new_cmd" USR2
